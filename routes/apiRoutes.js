49cf6c0ce22316d05e6e4e141fe9e916a1cfd601
Mona Lisa <giatran1222@gmail.com>
2022-12-13 21:52:20 -0800

    delete working cant render to page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,38 +8,38 @@ const {
 
 // GET /api/notes read db.json file and return all saved notes
 api.get('/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 });
 
 // POST /api/notes
 api.post('/notes', (req, res) => {
-  console.log(req.body);
-  const { title, text } = req.body;
+    console.log(req.body);
+    const { title, text } = req.body;
 
-  if(req.body) {
-    const newNote = {
-      title,
-      text,
-      noteId: uuidv4(),
-    };
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            noteId: uuidv4(),
+        };
 
-    readAndAppend(newNote, './db/db.json');
-    res.json('Note addd successfully');
-  } else {
-    res.json('Error in adding note');
-  }
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note addd successfully');
+    } else {
+        res.json('Error in adding note');
+    }
 });
 
 // DELETE /api/notes/:id
-api.delete('/notes/noteId', (req, res) => {
-    const noteId = req.params.noteId;
+api.delete('/notes/:id', (req, res) => {
+    const id = req.params.noteId;
     readFromFile('./db/db.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
-            const result = json.filter((note) => note.note_id !== noteId);
+            const result = json.filter((note) => note.noteId !== id);
 
             writeToFile('./db/db.json', result);
-            res.json(`Note ${noteId} has been deleted`);
+            res.json(`Note ${id} has been deleted`);
         });
 });
 
